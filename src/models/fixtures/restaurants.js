b31253f297691aa92f54439594e5441c92082cb7
Josh Hoff <jhoff@bitovi.com>
2018-11-12 15:18:52 -0600

    unit test restaurant list component
    sections completed:
    - (all here) https://donejs.com/place-my-order.html#creating-a-unit-tested-view-model

--- src/models/fixtures/restaurants.js
+++ src/models/fixtures/restaurants.js
@@ -1,14 +1,40 @@
-import fixture from 'can-fixture';
-import Restaurant from '../restaurant';
+import fixture from "can-fixture";
+import Restaurant from "../restaurant";
 
-const store = fixture.store([{
-  _id: 0,
-  description: 'First item'
-}, {
-  _id: 1,
-  description: 'Second item'
-}], Restaurant.connection.algebra);
+const store = fixture.store(
+  [
+    {
+      _id: 1,
+      name: "Cheese City",
+      slug: "cheese-city",
+      address: {
+        city: "Casadina",
+        state: "CA"
+      },
+      images: {
+        banner: "node_modules/place-my-order-assets/images/1-banner.jpg",
+        owner: "node_modules/place-my-order-assets/images/2-owner.jpg",
+        thumbnail: "node_modules/place-my-order-assets/images/3-thumbnail.jpg"
+      }
+    },
+    {
+      _id: 2,
+      name: "Crab Barn",
+      slug: "crab-barn",
+      address: {
+        city: "Alberny",
+        state: "NT"
+      },
+      images: {
+        banner: "node_modules/place-my-order-assets/images/2-banner.jpg",
+        owner: "node_modules/place-my-order-assets/images/3-owner.jpg",
+        thumbnail: "node_modules/place-my-order-assets/images/2-thumbnail.jpg"
+      }
+    }
+  ],
+  Restaurant.connection.algebra
+);
 
-fixture('/api/restaurants/{_id}', store);
+fixture("/api/restaurants/{_id}", store);
 
 export default store;
--- src/restaurant/list/list.js
+++ src/restaurant/list/list.js
@@ -3,12 +3,46 @@ import DefineMap from "can-define/map/";
 import "./list.less";
 import view from "./list.stache";
 import Restaurant from "~/models/restaurant";
+import State from "~/models/state";
+import City from "~/models/city";
 
 export const ViewModel = DefineMap.extend({
-  restaurants: {
-    value() {
-      return Restaurant.getList({});
+  get states() {
+    return State.getList({});
+  },
+  state: {
+    type: "string",
+    default: null,
+    set() {
+      // Remove the city when the state changes
+      this.city = null;
     }
+  },
+  get cities() {
+    let state = this.state;
+
+    if (!state) {
+      return null;
+    }
+
+    return City.getList({ state });
+  },
+  city: {
+    type: "string",
+    default: null
+  },
+  get restaurants() {
+    let state = this.state;
+    let city = this.city;
+
+    if (state && city) {
+      return Restaurant.getList({
+        "address.state": state,
+        "address.city": city
+      });
+    }
+
+    return null;
   }
 });
 
